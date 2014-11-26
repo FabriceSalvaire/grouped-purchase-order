ef2bad889159941b344808cb88179135d3908f19
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2014-11-26 10:04:22 +0100

    Add missing file

---
+++ GroupedPurchaseOrder/api.py
@@ -0,0 +1,53 @@
+####################################################################################################
+# 
+# GroupedPurchaseOrder - A Django Application.
+# Copyright (C) 2014 Fabrice Salvaire
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+# 
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+####################################################################################################
+
+####################################################################################################
+
+from tastypie.authentication import SessionAuthentication, ApiKeyAuthentication
+from tastypie.resources import ModelResource
+from tastypie.authorization import DjangoAuthorization, ReadOnlyAuthorization
+
+####################################################################################################
+
+from .models import Order
+
+####################################################################################################
+
+class OrderResource(ModelResource):
+
+    class Meta:
+        queryset = Order.objects.all()
+        resource_name = 'order'
+        # authentication = ApiKeyAuthentication()
+        authentication = SessionAuthentication()
+        authorization = ReadOnlyAuthorization()
+
+    ##############################################
+
+    def dehydrate(self, bundle):
+
+        bundle.data['name'] = bundle.obj.name()
+        return bundle
+
+####################################################################################################
+# 
+# End
+# 
+####################################################################################################
