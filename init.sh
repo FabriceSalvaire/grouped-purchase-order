12970e9247c651e9b39cad1250881ee281506d4e
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2016-01-14 18:26:57 +0100

    fixed init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
-python manage.py migrate
-python manage.py createsuperuser
-python manage.py makemigrations GroupedPurchaseOrder
-python manage.py migrate
-python manage.py runserver
+rm db.sqlite3
+rm GroupedPurchaseOrder/migrations/*.py
+
+manage.py makemigrations
+manage.py migrate
+manage.py makemigrations GroupedPurchaseOrder
+manage.py makemigrations django_messages # ?
+manage.py migrate
+manage.py loaddata GroupedPurchaseOrder/fixtures/demo.json
+sqlite3 db.sqlite3 < tools/set-superuser.sql
+
+# manage.py createsuperuser
+# manage.py runserver
